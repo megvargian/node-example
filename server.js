c61e6b1592e35aa36d0956211dd0a338f610068d
megvargian <mikekouyoumdjian@hotmail.com>
2020-11-28 23:24:39 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/Views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  let log = `${now}: ${req.method} , ${req.url}`;
+  
+  console.log(log);
+  fs.appendFile('server.log', log +'\n', (err) => {
+      if(err){
+          console.log('Unable to append to server.log')
+      }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+    res.render('Maintenance.hbs', {
+        pageTitle:'stop',
+        message: 'koll kara'
+    })
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+   res.render('home.hbs', {
+       pageTitle: 'Home Page',
+       message: 'Welcome to my website',
+   })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle:'About Page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessgae: 'this is a bad Page'
+    })
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is up on Prot 3000');
+});
